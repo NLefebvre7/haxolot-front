d373ef16e4f858d67cf5d493adc79cd806e67f1b
NLefebvre7 <57395815+NLefebvre7@users.noreply.github.com>
2020-11-27 07:31:54 +0100

    base front, aucun front mais console log et debut get axios
    base front
    recupere console log app js mais pas plus
    debut du get axios

---
+++ src/app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const server = express();
+
+const hostname = '0.0.0.0';
+const port = 3000;                        
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded());
+server.use(bodyParser.json());
+
+const axios = require('axios')
+
+
+
+server.get("/", (req, res) => {
+  res.json({ message: "Haxolot FRONT application." });
+
+ 
+//  axios
+//     .get('localhost:3000/schools/all', {
+//       headers: {
+//         accept: 'application/json',
+//         'content-type': 'application/json',
+//       },
+//     })
+//     .then(function(response) {
+//       myWorkspace.push(response.data['workspaces']);
+//       return myWorkspace;
+//     })
+//     .catch(function(error) {
+//       // handle error
+//       console.log(error); 
+//     })
+//     .then(function() {
+//       // always executed
+//     });
+
+//   res.render('fullscreen', {
+//     workspace: JSON.stringify(myWorkspace),
+//   });
+
+
+});
+
+
+console.log("app js front");
+server.listen(port, hostname);
